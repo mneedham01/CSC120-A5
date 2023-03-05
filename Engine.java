f0ef1369ceae12cc1ec5eeecb27e43347c3ba792
mneedham01 <mneedham@smith.edu>
2023-03-05 13:11:52 -0500

    got engine to work

--- Engine.java
+++ Engine.java
@@ -1,3 +1,53 @@
 public class Engine {
 
+    private FuelType fuelType;
+    double currentFuelLevel;
+    double maxFuelLevel;
+
+    //constructor
+    public Engine(FuelType fuelType, double maxFuelLevel){
+        this.fuelType=fuelType;
+        this.currentFuelLevel=maxFuelLevel;
+        this.maxFuelLevel=maxFuelLevel;
+    }
+
+    //accessors
+    public FuelType getFuelType(){
+        return this.fuelType;
+    }
+    public double getCurrentFuelType(){
+        return this.currentFuelLevel;
+    }
+    public double getMaxFuelLevel(){
+        return this.maxFuelLevel;
+    }
+
+    //methods
+    public void refuel(){
+        this.currentFuelLevel=this.maxFuelLevel;
+    }
+    public void go(){
+        if (this.currentFuelLevel==0){
+            throw new RuntimeException("Can't go. Fuel Level is at 0.");
+        }
+        if ((this.currentFuelLevel-this.maxFuelLevel/4)<0){
+            throw new RuntimeException("Can't go. Fuel Level is too low to go.");
+        }
+        else{
+            this.currentFuelLevel=this.currentFuelLevel-this.maxFuelLevel/4;
+            System.out.println("Success! The new fuel level is: "+this.currentFuelLevel);
+        }
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+    }
+
 }
