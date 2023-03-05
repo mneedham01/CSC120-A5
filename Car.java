13cae5ac6eed5b55c5c1d58295237fadbb2089b2
mneedham01 <mneedham@smith.edu>
2023-03-05 15:47:02 -0500

    changed array list

--- Car.java
+++ Car.java
@@ -1,3 +1,56 @@
+import java.util.ArrayList;
+
+import javax.management.RuntimeErrorException;
+
 public class Car {
+
+    //initialize variables
+    private ArrayList passengersOnboard;
+    private int maxCapacity;
+
+    //constructor
+    public Car(int maxCapacity){
+        this.maxCapacity= maxCapacity;
+        ArrayList passengersOnboard= new ArrayList();
+    }
+
+    //accessor like methods
+    public int getCapacity(){
+        return this.maxCapacity;
+    }
+    public int seatsRemaining(){
+        return this.maxCapacity-passengersOnboard.size();
+    }
+
+    //add a passenger
+    public void addPassenger(Passenger p){
+        if (seatsRemaining()==0){
+            throw new RuntimeErrorException(null, "No more seats remaining. Can't add passenger.");
+        }
+        else{
+            passengersOnboard.add(p);
+        }
+    }
+    //remove a passenger
+    public void removePassenger(Passenger p){
+        if (!passengersOnboard.contains(p)){
+            throw new RuntimeErrorException(null, "Passenger is not on the car.");
+        }
+        else{
+            passengersOnboard.remove(passengersOnboard.indexOf(p));
+        }
+    }
+    //print manifest
+    public void printManifest(){
+        if (passengersOnboard.size()==0){
+            System.out.println("This car is EMPTY!");
+        }
+        else{
+            for (Object i: passengersOnboard){
+                System.out.println(i);
+            }
+        }
+    }
+
 
 }
