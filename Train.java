5e794a42ebcd515059df50749a6e2dde7fe4145f
mneedham01 <mneedham@smith.edu>
2023-03-06 22:11:16 -0500

    added javadoc

--- Train.java
+++ Train.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/*
+ * Train constructor, can get engine, get a specific car, the maximum capacity of train, the total seats left 
+ * in the train, and print a list of passengers in the train. 
+ * @param: FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity
+ */
 public class Train {
 
     private final Engine engine;
@@ -20,14 +25,27 @@ public class Train {
 
     }
     
+    /*
+     * Returns the engine
+     * @return: this.engine
+     */
     public Engine getEngine(){
         return this.engine;
     }
 
+    /*
+     * Returns the car for a given index number. 
+     * @param: int i 
+     * @returns: car 
+     */
     public Car getCar(int i){
         return this.cars.get(i);
     }
-
+    
+    /*
+     * Returns the maximum number of passengers in the train
+     * @returns: max_count 
+     */
     public int getMaxCapacity(){
         max_count=0;
         for (Car i: this.cars){
@@ -36,6 +54,10 @@ public class Train {
         return max_count;
     }
 
+    /* 
+     * Returns maximum number of seats remaining in a train. 
+     * @returns: seat_count 
+     */
     public int seatsRemaining(){
         seat_count=0;
         for (Car i: this.cars){
@@ -44,6 +66,9 @@ public class Train {
         return seat_count;
     }
 
+    /*
+     * Prints out a list of the passengers in the train. 
+     */
     public void printManifest(){
         System.out.println("These are the passengers in the train: ");
         for (Car i: this.cars){
