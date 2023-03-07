87fb7c1fae477b94b0b9d4978b8e6b31e9c0a027
mneedham01 <mneedham@smith.edu>
2023-03-06 19:02:09 -0500

    rough draft for train

--- Train.java
+++ Train.java
@@ -1,3 +1,49 @@
+import java.util.ArrayList;
+
 public class Train {
+
+    private final Engine engine;
+    private ArrayList cars; 
+    public int max_count;
+    public int seat_count;
+
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity){
+        this.fuelCapacity=fuelCapacity;
+        this.passengerCapacity=passengerCapacity;  
+
+        this.Engine=Engine(fuelType, fuelCapacity);
+
+       // for (Object i: cars){
+            //cars.add(i);
+       // }
+
+    }
+    
+    public Engine getEngine(){
+        return this.Engine();
+    }
+
+    public Object getCar(int i){
+        return cars.get(i);
+    }
+
+    public int getMaxCapacity(){
+        max_count=0;
+        for (Object i:cars){
+            max_count += cars.get(i).getCapacity();
+        }
+        return max_count;
+    }
+
+    public int seatsRemaining(){
+        seat_count=0;
+        for (Object i: cars){
+            seat_count+=cars.get(i).seatsRemaining();
+        }
+        return seat_count;
+    }
+
+
+
 
 }
