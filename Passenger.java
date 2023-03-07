2d4946379545fa77529672e3c7d4e69313cf64a8
mneedham01 <mneedham@smith.edu>
2023-03-06 19:57:23 -0500

    edited passenger

--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,25 @@
+import java.lang.invoke.TypeDescriptor;
+
 public class Passenger {
     
     private String name;
 
     public Passenger(String name) {
         this.name = name;
+    }
+
+    public void boardCar(Car c){
+        try{
+            c.addPassenger(this);
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+    public void getOffCar(Car c){
+        try{
+            c.removePassenger(this);
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 }
